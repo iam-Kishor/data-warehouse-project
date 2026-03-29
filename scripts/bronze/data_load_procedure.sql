00db3a60b3ae7465a85923a7439866f9a89963fb
Sai Kishore Praveen Kumar Thadi <praveenkumarthadi143@gmail.com>
2026-03-29 20:39:42 +0530

    Add DDL script header comment for bronze tables
    Add a header comment to the data load procedure script.

--- scripts/bronze/data_load_procedure.sql
+++ scripts/bronze/data_load_procedure.sql
@@ -1,3 +1,13 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
 CREATE OR ALTER PROCEDURE bronze.batchload AS
 BEGIN
 	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
